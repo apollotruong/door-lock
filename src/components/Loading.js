3c2ba2b2943e59adc7341e87269c654a60edda1e
Apollo Truong <atruo017@ucr.edu>
2020-03-09 14:48:08 -0700

    removed auth_config and experimenting with profile assets

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,9 +2,19 @@ import React from "react";
 import loading from "../assets/loading.svg";
 
 const Loading = () => (
-  <div className="spinner">
+  <div className="spinner" class="styles.center">
     <img src={loading} alt="Loading" />
+    <br />
+    This may take a while!
   </div>
 );
 
 export default Loading;
+
+var styles = `center {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
+}
+`;
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,6 +2,7 @@
 
 import React, { Fragment } from "react";
 import { useAuth0 } from "../react-auth0-spa";
+import { ProfilePic } from "../assets/ProfilePic.jpg";
 
 const Profile = () => {
   const { loading, user } = useAuth0();
@@ -12,7 +13,10 @@ const Profile = () => {
 
   return (
     <Fragment>
-      <img src={user.picture} alt="Profile" />
+      {/* <img src={user.picture} alt="Profile" /> */}
+      <center>
+        <img src={ProfilePic} alt="Profile" />
+      </center>
 
       <h2>{user.name}</h2>
       <p>{user.email}</p>
